0da0f1a902d775228cd764ad1be1fddad4235650
Aslan <mindlesswater@gmail.com>
2021-11-22 06:03:54 +0300

    Finish

--- main.py
+++ main.py
@@ -1,37 +1,50 @@
-from random import randint
+from random import choice
 class Game():
 
-    step={'к':'камень','p':'бумага','s':'ножницы'}
+    step={'r':'камень','p':'бумага','s':'ножницы'}
     save_step_pc=0
     save_step_player=0
 
     def pc_action(self):
-        self.pcaction=randint(1,3)
+        global save_step_pc
+        self.pcaction=choice(['r','p','s'])
         for x,y in self.step.items():
             if self.pcaction==x:
-                global save_number_pc
-                save_number_pc=x
+                self.save_step_pc=x
                 self.pcaction=y
-                return f'Компьютер бросил {self.pcaction}!'
+                return f'Компьютер бросил {self.pcaction}! \n'
 
 
 
     def player_action(self):
+        global save_step_player
         self.player_throw=input(str('Вы бросаете : ')).lower()
         for x,y in self.step.items():
             if self.player_throw==y:
-                global save_step_player
-                save_step_player=x
+                self.save_step_player=x
                 return f'Вы бросили {self.player_throw}! \n'
-        raise Exception ('Вы должны выбросить камень ножницы или бумагу!')
+        if self.save_step_player!=self.player_throw:
+            print('Вы должны бросить камень, ножницы или бумагу!')
+            return self.player_action()
+
+
 
     def start_game(self):
-        print('Начинаем игру... ',end='\n')
-        print(self.player_action())
-        print(self.pc_action())
-        if self.save_step_player==self.save_step_pc:
-            return 'Ничья!'
-
-
+            print('Начинаем игру... ',end='\n')
+            print(self.player_action())
+            print(self.pc_action())
+            if self.save_step_player == self.save_step_pc:
+                return 'Ничья!\n'
+            elif (self.save_step_player == 'r' and self.save_step_pc=='s') or (self.save_step_player=='s' and self.save_step_pc=='p') or (self.save_step_player== 'p' and self.save_step_pc=='r'):
+                return 'Вы выиграли! \n'
+            else:
+                return 'Вы проиграли!\n'
+play=Game()
+while True:
+    print(play.start_game())
+    ask=input(str('Ещё одну игру? пиши "да" если хочешь реванш : ')).lower()
+    if ask!='да':
+        print('Пока!')
+        break
 
 
